1f508b4e89cf3b2c2b1f5c0ababce52dda687c3c
sudeshmoreyos <108395648+sudeshmoreyos@users.noreply.github.com>
2022-11-11 21:01:05 +0530

    Delete arch/AVR/MEGA/drivers/backups directory

--- arch/AVR/MEGA/drivers/backups/i2c_user.h
+++
@@ -1,8 +0,0 @@
-#ifndef _i2c_user_h
-#define _i2c_user_h
-void i2c_init1();
-unsigned char i2c_start1();
-unsigned char i2c_write1(unsigned char data);
-unsigned char i2c_read1(unsigned char * data, int acknowledge);
-void i2c_stop1();
-#endif
