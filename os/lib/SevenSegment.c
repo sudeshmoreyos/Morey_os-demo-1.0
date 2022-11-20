7d83ffca911ccd3c581f08dbd0ffb73c2d105b3c
sudeshmoreyos <108395648+sudeshmoreyos@users.noreply.github.com>
2022-11-20 19:50:51 +0530

    SevenSegment Lib Version-2

--- os/lib/SevenSegment.c
+++ os/lib/SevenSegment.c
@@ -15,42 +15,66 @@
 
 const mos_uint8_t digit_values[19] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71, 0x80, 0x40, 0x00};
 
-void SevenSegment_begin (struct seven_segment_struct * seven_segment_obj, mos_uint8_t segment_type, mos_uint8_t pina, mos_uint8_t pinb, \
-	mos_uint8_t pinc, mos_uint8_t pind,mos_uint8_t pine, mos_uint8_t pinf,mos_uint8_t ping, mos_uint8_t pinh)
-{
-	mos_uint8_t i;
-	
+void SevenSegment_begin (struct seven_segment_struct * seven_segment_obj, mos_uint8_t segment_type)
+{	
 	seven_segment_obj->segment_type =  segment_type;
-	seven_segment_obj->segment_pins[0] =  pina;
-	seven_segment_obj->segment_pins[1] =  pinb;
-	seven_segment_obj->segment_pins[2] =  pinc;
-	seven_segment_obj->segment_pins[3] =  pind;
-	seven_segment_obj->segment_pins[4] =  pine;
-	seven_segment_obj->segment_pins[5] =  pinf;
-	seven_segment_obj->segment_pins[6] =  ping;
-	seven_segment_obj->segment_pins[7] =  pinh;
 	
-	for(i=0;i<8;i++)
-	{
-		if(seven_segment_obj->segment_pins[i] != NOT_USED_PIN )
-			Digital.pinmode(seven_segment_obj->segment_pins[i],OUTPUT);
-	}	
+	if(seven_segment_obj->segment_pin_a != NOT_USED_PIN )
+		Digital.pinmode(seven_segment_obj->segment_pin_a,OUTPUT);
+
+	if(seven_segment_obj->segment_pin_b != NOT_USED_PIN )
+		Digital.pinmode(seven_segment_obj->segment_pin_b,OUTPUT);
+	
+	if(seven_segment_obj->segment_pin_c != NOT_USED_PIN )
+		Digital.pinmode(seven_segment_obj->segment_pin_c,OUTPUT);
+
+	if(seven_segment_obj->segment_pin_d != NOT_USED_PIN )
+		Digital.pinmode(seven_segment_obj->segment_pin_d,OUTPUT);
+
+	if(seven_segment_obj->segment_pin_e != NOT_USED_PIN )
+		Digital.pinmode(seven_segment_obj->segment_pin_e,OUTPUT);
+
+	if(seven_segment_obj->segment_pin_f != NOT_USED_PIN )
+		Digital.pinmode(seven_segment_obj->segment_pin_f,OUTPUT);
+	
+	if(seven_segment_obj->segment_pin_g != NOT_USED_PIN )
+		Digital.pinmode(seven_segment_obj->segment_pin_g,OUTPUT);
+
+	if(seven_segment_obj->segment_pin_h != NOT_USED_PIN )
+		Digital.pinmode(seven_segment_obj->segment_pin_h,OUTPUT);	
+}
+
+static void digit_value_print(struct seven_segment_struct * seven_segment_obj, mos_uint8_t digit_value)
+{
+	if(seven_segment_obj->segment_pin_a != NOT_USED_PIN )
+		Digital.write(seven_segment_obj->segment_pin_a, (digit_value & (1<<0))>>0);	
+	if(seven_segment_obj->segment_pin_b != NOT_USED_PIN )
+		Digital.write(seven_segment_obj->segment_pin_b, (digit_value & (1<<1))>>1);	
+	if(seven_segment_obj->segment_pin_c != NOT_USED_PIN )
+		Digital.write(seven_segment_obj->segment_pin_c, (digit_value & (1<<2))>>2);	
+	if(seven_segment_obj->segment_pin_d != NOT_USED_PIN )
+		Digital.write(seven_segment_obj->segment_pin_d, (digit_value & (1<<3))>>3);
+	if(seven_segment_obj->segment_pin_e != NOT_USED_PIN )
+		Digital.write(seven_segment_obj->segment_pin_e, (digit_value & (1<<4))>>4);	
+	if(seven_segment_obj->segment_pin_f != NOT_USED_PIN )
+		Digital.write(seven_segment_obj->segment_pin_f, (digit_value & (1<<5))>>5);	
+	if(seven_segment_obj->segment_pin_g != NOT_USED_PIN )
+		Digital.write(seven_segment_obj->segment_pin_g, (digit_value & (1<<6))>>6);	
+	if(seven_segment_obj->segment_pin_h != NOT_USED_PIN )
+		Digital.write(seven_segment_obj->segment_pin_h, (digit_value & (1<<7))>>7);		
+	
 }
 
 void SevenSegment_print (struct seven_segment_struct * seven_segment_obj, mos_uint8_t digit)
 {
-	mos_uint8_t j, digit_value=0;
+	mos_uint8_t digit_value=0;
 	
 	if(seven_segment_obj->segment_type == CATHODE)
 		digit_value = digit_values[digit];
 	else
 		digit_value = ~digit_values[digit];
-    
-	for(j=0;j<8;j++) 
-	{
-		if(seven_segment_obj->segment_pins[j] != NOT_USED_PIN )
-			Digital.write(seven_segment_obj->segment_pins[j], (digit_value & (1<<j))>>j);
-	}	
+	
+	digit_value_print(seven_segment_obj,digit_value);	
 }
 
 void SevenSegment_printDot (struct seven_segment_struct * seven_segment_obj, mos_uint8_t digit)
@@ -58,14 +82,14 @@ void SevenSegment_printDot (struct seven_segment_struct * seven_segment_obj, mos
 	SevenSegment_print (seven_segment_obj, digit);
 	
 	if(seven_segment_obj->segment_type == CATHODE)
-		Digital.write(seven_segment_obj->segment_pins[7], HIGH);		
+		Digital.write(seven_segment_obj->segment_pin_h, HIGH);		
 	else
-		Digital.write(seven_segment_obj->segment_pins[7], LOW);	
+		Digital.write(seven_segment_obj->segment_pin_h, LOW);	
 }
 
 void SevenSegment_printReverse (struct seven_segment_struct * seven_segment_obj, mos_uint8_t digit)
 {
-	mos_uint8_t a,b,digit_value=0,j=0;
+	mos_uint8_t a,b,digit_value=0;
 	
 	if(seven_segment_obj->segment_type == CATHODE)
 		digit_value = digit_values[digit];
@@ -82,11 +106,7 @@ void SevenSegment_printReverse (struct seven_segment_struct * seven_segment_obj,
 		}
 	}
     
-	for(j=0;j<8;j++) 
-	{
-		if(seven_segment_obj->segment_pins[j] != NOT_USED_PIN )
-			Digital.write(seven_segment_obj->segment_pins[j], (digit_value & (1<<j))>>j);
-	}	
+	digit_value_print(seven_segment_obj,digit_value);	
 }
 
 void SevenSegment_printDotReverse (struct seven_segment_struct * seven_segment_obj, mos_uint8_t digit)
@@ -94,35 +114,69 @@ void SevenSegment_printDotReverse (struct seven_segment_struct * seven_segment_o
 	SevenSegment_printReverse (seven_segment_obj, digit);
 	
 	if(seven_segment_obj->segment_type == CATHODE)
-		Digital.write(seven_segment_obj->segment_pins[7], HIGH);		
+		Digital.write(seven_segment_obj->segment_pin_h, HIGH);		
 	else
-		Digital.write(seven_segment_obj->segment_pins[7], LOW);
+		Digital.write(seven_segment_obj->segment_pin_h, LOW);
 }
 
-void SevenSegment_customPrint (struct seven_segment_struct * seven_segment_obj, mos_uint8_t * custom_data)
+void SevenSegment_customPrint (struct seven_segment_struct * seven_segment_obj, \
+mos_uint8_t custom_data_pin_a, mos_uint8_t custom_data_pin_b, mos_uint8_t custom_data_pin_c, mos_uint8_t custom_data_pin_d, \
+mos_uint8_t custom_data_pin_e, mos_uint8_t custom_data_pin_f, mos_uint8_t custom_data_pin_g, mos_uint8_t custom_data_pin_h)
 {
-	mos_uint8_t j;		
-
-	for(j=0;j<8;j++)
+	mos_uint8_t digit_value=0;
+	
+	if(custom_data_pin_h > 0)
 	{
-		if(seven_segment_obj->segment_pins[j] != NOT_USED_PIN )
-		{
-			if(seven_segment_obj->segment_type == CATHODE)
-			{
-				if(custom_data[j] == 0)
-					Digital.write(seven_segment_obj->segment_pins[j], LOW);
-				else
-					Digital.write(seven_segment_obj->segment_pins[j], HIGH);
-			}
-			else
-			{
-				if(custom_data[j] == 0)
-					Digital.write(seven_segment_obj->segment_pins[j], HIGH);
-				else
-					Digital.write(seven_segment_obj->segment_pins[j], LOW);
-			}
-		}
+		digit_value++;		
 	}
+	digit_value <<= 1;
+	
+	if(custom_data_pin_g > 0)
+	{
+		digit_value++;
+	}
+	digit_value <<= 1;
+	
+	if(custom_data_pin_f > 0)
+	{
+		digit_value++;
+	}
+	digit_value <<= 1;
+	
+	if(custom_data_pin_e > 0)
+	{
+		digit_value++;
+	}
+	digit_value <<= 1;
+	
+	if(custom_data_pin_d > 0)
+	{
+		digit_value++;		
+	}
+	digit_value <<= 1;
+	
+	if(custom_data_pin_c > 0)
+	{
+		digit_value++;
+	}
+	digit_value <<= 1;
+	
+	if(custom_data_pin_b > 0)
+	{
+		digit_value++;
+	}
+	digit_value <<= 1;
+	
+	if(custom_data_pin_a > 0)
+	{
+		digit_value++;
+	}
+	
+	if(seven_segment_obj->segment_type == ANODE)
+		digit_value = ~digit_value;
+	
+	digit_value_print(seven_segment_obj, digit_value);
+	
 }
 
 
