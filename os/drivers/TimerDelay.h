5e785e80e8c9c0192550ff6027fbe436450e0c62
sudeshmoreyos <108395648+sudeshmoreyos@users.noreply.github.com>
2022-12-04 11:30:41 +0530

    Added TimerDelay.end function

--- os/drivers/TimerDelay.h
+++ os/drivers/TimerDelay.h
@@ -5,7 +5,8 @@
 
 struct timer_delay_driver
 {
-	void (*begin) (mos_uint8_t timer_no , float timer_delay_ms, void (*timer_delay_callback)(void));	
+	void (*begin) (mos_uint8_t timer_no , float timer_delay_ms, void (*timer_delay_callback)(void));
+	void (*end) (mos_uint8_t timer_no);
 };
 
 #include "../../arch/arch_timer_delay.h"
