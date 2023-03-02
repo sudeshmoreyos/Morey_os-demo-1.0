b08b9a0959d13e15c1f53fe80a056ea258af06a8
sudeshmoreyos <108395648+sudeshmoreyos@users.noreply.github.com>
2023-03-03 00:00:03 +0530

    Update SevenSegmentMulti.h

--- os/lib/SevenSegmentMulti.h
+++ os/lib/SevenSegmentMulti.h
@@ -6,18 +6,26 @@
 #ifdef PLATFORM_SUPPORT_DIGITAL_IO
 #include "SevenSegment.h"
 
+#define NORMAL_MODE 0
+#define INVERTED_MODE 1
+
 typedef struct seven_segment_multi_struct 
 {
 	SevenSegment_t * seven_segment_obj;
 	mos_uint8_t segment_count;
 	mos_uint8_t * segment_common_pins;
 	mos_uint8_t digit_display[8];
+	mos_uint8_t dot_display[8];
+	mos_uint8_t reverse_display[8];
 	mos_uint8_t count;
+	mos_uint8_t mode;
 } SevenSegmentMulti_t;
 
 void SevenSegmentMulti_begin (SevenSegmentMulti_t * seven_segment_multi_obj, SevenSegment_t * seven_segment_obj, \
 	mos_uint8_t segment_count, mos_uint8_t * segment_common_pins);
+void SevenSegmentMulti_config(SevenSegmentMulti_t * seven_segment_multi_obj, mos_uint8_t mode);
 void SevenSegmentMulti_print (SevenSegmentMulti_t * seven_segment_multi_obj, mos_uint16_t number);
+void SevenSegmentMulti_printReverse (SevenSegmentMulti_t * seven_segment_multi_obj, mos_uint16_t number, mos_uint8_t * reverse_position);
 void SevenSegmentMulti_printDot(SevenSegmentMulti_t * seven_segment_multi_obj, mos_uint16_t number,\
 	mos_uint8_t * dot_position);
 void SevenSegmentMulti_printDotReverse(SevenSegmentMulti_t * seven_segment_multi_obj, mos_uint16_t number,\
