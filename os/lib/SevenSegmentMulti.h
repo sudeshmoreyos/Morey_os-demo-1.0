1d6381fb65f3d4fb4379e428d69748fc19debb6b
sudeshmoreyos <108395648+sudeshmoreyos@users.noreply.github.com>
2026-05-19 11:43:52 +0200

    Update SevenSegmentMulti.h
    Resolved a bug which only takes mos_uint16_t size numbers to display. Updated it to mos_uint32_t size.

--- os/lib/SevenSegmentMulti.h
+++ os/lib/SevenSegmentMulti.h
@@ -24,11 +24,11 @@ void SevenSegmentMulti_begin (SevenSegmentMulti_t * seven_segment_multi_obj, Sev
 	mos_uint8_t * segment_common_pins, mos_uint8_t segment_count);
 void SevenSegmentMulti_callback(SevenSegmentMulti_t * seven_segment_multi_obj);
 void SevenSegmentMulti_config(SevenSegmentMulti_t * seven_segment_multi_obj, mos_uint8_t mode);
-void SevenSegmentMulti_print (SevenSegmentMulti_t * seven_segment_multi_obj, mos_uint16_t number);
-void SevenSegmentMulti_printReverse (SevenSegmentMulti_t * seven_segment_multi_obj, mos_uint16_t number, mos_uint8_t * reverse_position);
-void SevenSegmentMulti_printDot(SevenSegmentMulti_t * seven_segment_multi_obj, mos_uint16_t number,\
+void SevenSegmentMulti_print (SevenSegmentMulti_t * seven_segment_multi_obj, mos_uint32_t number);
+void SevenSegmentMulti_printReverse (SevenSegmentMulti_t * seven_segment_multi_obj, mos_uint32_t number, mos_uint8_t * reverse_position);
+void SevenSegmentMulti_printDot(SevenSegmentMulti_t * seven_segment_multi_obj, mos_uint32_t number,\
 	mos_uint8_t * dot_position);
-void SevenSegmentMulti_printDotReverse(SevenSegmentMulti_t * seven_segment_multi_obj, mos_uint16_t number,\
+void SevenSegmentMulti_printDotReverse(SevenSegmentMulti_t * seven_segment_multi_obj, mos_uint32_t number,\
 	mos_uint8_t * dot_position, mos_uint8_t * reverse_position);
 void SevenSegmentMulti_customPrint (struct seven_segment_multi_struct * seven_segment_multi_obj, mos_uint8_t * * custom_data);
 	
